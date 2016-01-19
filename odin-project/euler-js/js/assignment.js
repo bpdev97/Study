a0b47398fe2d0e9a5fc72600279b5409e6dc48c4
bpdev97 <bp101297@gmail.com>
2016-01-19 10:40:14 -0500

    Add lPMFactor method.

--- odin-project/euler-js/js/assignment.js
+++ odin-project/euler-js/js/assignment.js
@@ -26,8 +26,17 @@ var evenFib = function () {
 }
 
 var lPFactor = function () {
-  var number = document.getElementById("p3Num");
-
+  var number = document.getElementsByName("p3Num")[0].value;
+  var result = document.getElementById("result3");
+  var temp = 2;
+  while(temp * temp <= number)
+  {
+    if (number % temp === 0)
+      number /= temp;
+    else
+      temp++;
+  }
+  result.innerHTML = number;
 }
 
 var setProb = function (button) {
@@ -35,23 +44,32 @@ var setProb = function (button) {
   var prob1 = document.getElementById("prob-1");
   var prob2 = document.getElementById("prob-2");
   var prob3 = document.getElementById("prob-3");
+  var but1 = document.getElementsByName("assignment1")[0];
+  var but2 = document.getElementsByName("assignment2")[0];
+  var but3 = document.getElementsByName("assignment3")[0];
 
   //reset ui
   prob1.style.display = "none";
   prob2.style.display = "none";
   prob3.style.display = "none";
+  but1.style.color = "white";
+  but2.style.color = "white";
+  but3.style.color = "white";
 
   //setup ui
   if(button === 1) {
     prob1.style.display = "block";
+    but1.style.color = "#b3b1b1";
   }
 
   else if(button === 2) {
     prob2.style.display = "block";
+    but2.style.color = "#b3b1b1";
   }
 
   else if(button === 3) {
     prob3.style.display = "block";
+    but3.style.color = "#b3b1b1";
   }
 
 }
