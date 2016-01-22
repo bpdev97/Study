90e09616b617ac3618b141a74c8e094a685296f8
bpdev97 <bp101297@gmail.com>
2016-01-21 22:17:53 -0500

    Add settings box event handlers

--- odin-project/etch-a-sketch/js/eas.js
+++ odin-project/etch-a-sketch/js/eas.js
@@ -2,11 +2,23 @@ var gSize = 20;
 
 $(document).ready(function(){
   createGrid(gSize);
+
+  //change ePixel color on mouseenter
   $(document).on('mouseenter','.e-pixel',function(event){
     $(this).css({'background-color': 'black'})
   })
+
+  //Clear grid when cButton is clicked
+  $('#cButton').click(clearGrid);
+
+  //update gSize and call clearGrid when rGButton is clicked
+  $('#rGButton').click(function(){
+    gSize = $('#gSizeD').val();
+    clearGrid();
+  });
 });
 
+//Create a grid of size gSize
 function createGrid(gSize){
   var pSize = 375/gSize + 'px';
   var $rDiv = $('<div class="e-column"></div>');
@@ -23,4 +35,10 @@ function createGrid(gSize){
   //Append rDiv clones to screen div
   for(i=0; i < gSize; i++)
     $('#screen').append($rDiv.clone());
+}
+
+//Pretty self explanatory
+function clearGrid(){
+  $('.e-column').remove();
+  createGrid(gSize);
 }
